0e91c58f4e4029f579dd1612a0b0568cde61672e
Carlos Daniel Morillo <morillocardan@gmail.com>
2026-06-19 00:09:20 +0200

    Controlador Estudiantes creado
    Se creó el controlador EstudiantesController y se agregó una lista estática en memoria con datos de prueba para gestionar estudiantes.

---
+++ APIS_WEB_MORE/Controllers/EstudiantesController.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using APIS_WEB_MORE.Models;
+
+namespace APIS_WEB_MORE.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EstudiantesController : ControllerBase
+    {
+        private static List<Estudiante> estudiantes = new()
+        {
+            new Estudiante
+            {
+                Id = 1,
+                Nombre = "Luis",
+                Apellido = "Fortuna",
+                Correo = "luis@ufhec.edu.do",
+                Carrera = "Ingenieria de Sistemas",
+                Edad = 22,
+                Promedio = 90,
+                Activo = true
+            },
+            new Estudiante
+            {
+                Id = 2,
+                Nombre = "Ana",
+                Apellido = "Perez",
+                Correo = "ana@ufhec.edu.do",
+                Carrera = "Administracion",
+                Edad = 20,
+                Promedio = 80,
+                Activo = true
+            }
+        };
+    }
+}
